d2b1457992aac9ccb47da719a29292c8ec37b1b9
Tramnvph14967 <94088002+Tramnvph14967@users.noreply.github.com>
2022-04-19 09:15:16 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -15,41 +15,41 @@ function App() {
   const [products, setProducts] = useState<ProductType[]>([]); //1
 
   useEffect(() => {
-        const getProduct = async () => {
-          const {data} = await axios.get('http://localhost:3001/products');
-          setProducts(data);
-        }
-        getProduct()
-  },[]);
+    const getProduct = async () => {
+      const { data } = await axios.get('http://localhost:3001/products');
+      setProducts(data);
+    }
+    getProduct()
+  }, []);
 
   const onHanderRemove = (id: any) => {
     confirm('ban muon xua khong')
-      axios.delete('http://localhost:3001/products/'+id);
-      setProducts(products.filter(item => item.id !== id));
-      alert('xoa thanh cong')
+    axios.delete('http://localhost:3001/products/' + id);
+    setProducts(products.filter(item => item.id !== id));
+    alert('xoa thanh cong')
   }
 
-  const onhanderAdd = (data : ProductType) => {
-      axios.post('http://localhost:3001/products', data);
-      setProducts([...products, data]);
-      alert("them thanh cong")
+  const onhanderAdd = (data: ProductType) => {
+    axios.post('http://localhost:3001/products', data);
+    setProducts([...products, data]);
+    alert("them thanh cong")
   }
 
   const onHanderEdit = (data: ProductType) => {
-        axios.put('http://localhost:3001/products/'+data.id, data);
-        setProducts(products.map(item => item.id === data.id ? data: item));
-        alert('update thanh cong');
+    axios.put('http://localhost:3001/products/' + data.id, data);
+    setProducts(products.map(item => item.id === data.id ? data : item));
+    alert('update thanh cong');
   }
   return (
     <div className="App">
-        <Routes>
-          <Route path='/' element={<h1>Home page</h1>} />
-          <Route path='/products' element={<ListProducts products={products} onRemove={onHanderRemove}/>} />
-          <Route path='/products/add' element={<AddProducts onAdd={onhanderAdd}/>} />
-          <Route path='/products/:id/edit' element={<EditProducts onEdit={onHanderEdit}/>} />
-          <Route path='/signup' element={<Signup/>} />
-          <Route path='/signin' element={<Signin/>} />
-        </Routes>
+      <Routes>
+        <Route path='/' element={<h1>Home page</h1>} />
+        <Route path='/products' element={<ListProducts products={products} onRemove={onHanderRemove} />} />
+        <Route path='/products/add' element={<AddProducts onAdd={onhanderAdd} />} />
+        <Route path='/products/:id/edit' element={<EditProducts onEdit={onHanderEdit} />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/signin' element={<Signin />} />
+      </Routes>
     </div>
   )
 }
--- src/auth/Signin.tsx
+++ src/auth/Signin.tsx
@@ -1,41 +1,41 @@
 import React from 'react'
-import {useNavigate} from 'react-router-dom';
-import {useForm, SubmitHandler} from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { ProductType } from '../style/ProductsType';
 import axios from 'axios';
 import { Usertype } from '../style/UserType';
 
 
 type inputfom = {
-    id?: string,
-    name:string,
-    email: string,
-    password: number
+  id?: string,
+  name: string,
+  email: string,
+  password: number
 }
 const Signin = () => {
-    const navigete = useNavigate();
-    const {register, handleSubmit, formState: {errors}} = useForm<inputfom>();
+  const navigete = useNavigate();
+  const { register, handleSubmit, formState: { errors } } = useForm<inputfom>();
 
-    const onSubmit = async (datas : Usertype) =>{
-        const {data} = await axios.post('http://localhost:3001/signin', datas);
-        localStorage.setItem('user', JSON.stringify(data) );
+  const onSubmit = async (datas: Usertype) => {
+    const { data } = await axios.post('http://localhost:3001/signin', datas);
+    localStorage.setItem('user', JSON.stringify(data));
 
-    }
+  }
   return (
     <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-  <div className="mb-3">
-    <label  className="form-label">email</label>
-    <input type="text" {...register('email', {required: true})} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    {errors.email && <span>khong duoc de trong</span>}
-  </div>
-  <div className="mb-3">
-    <label  className="form-label">Pass</label>
-    <input type="number" {...register('password', {required: true})} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    {errors.password && <span>khong duoc de trong</span>}
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-3">
+          <label className="form-label">email</label>
+          <input type="text" {...register('email', { required: true })} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          {errors.email && <span>khong duoc de trong</span>}
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Pass</label>
+          <input type="number" {...register('password', { required: true })} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          {errors.password && <span>khong duoc de trong</span>}
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     </div>
   )
 }
